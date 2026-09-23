3033f363b9c5c455f71b106a27794689049e069c
skluber <arodriguezzmartinez@gmail.com>
2026-09-23 16:38:51 +0200

    Add general information section

---
+++ src/components/GeneralInformation.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import '../styles/GeneralInformation.css'
+
+export function GeneralInformation() {
+    const [ isEditing, setIsEditing ] = useState(true);
+
+    const [ generalInfo, setGeneralInfo ] = useState({
+        name: "Antonio", 
+        email: "antonio@gmail.com", 
+        phone: "655147917"
+    })
+
+
+    const handleChange = (event) => {
+        setGeneralInfo((previousInfo) => ({
+            ...previousInfo,
+            [event.target.id]: event.target.value
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setIsEditing(false);
+    }
+
+    const handleEdit = () => {
+        setIsEditing(true);
+    }
+
+    if (isEditing) {
+        return (
+            <form className="general-information" onSubmit={handleSubmit}>
+
+                <h1 className="section-title">General Information</h1>
+
+                <div className="section-inputs">
+
+                    <label htmlFor="name">Name</label>
+                    <input id="name" name="name" type="text" value={ generalInfo.name } onChange={handleChange} />
+
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" type="email" value={ generalInfo.email } onChange={handleChange} />
+
+                    <label htmlFor="phone">Phone number</label>
+                    <input id="phone" name="phone" type="tel" value={ generalInfo.phone } onChange={handleChange} />
+
+                </div>  
+
+                <button type="submit">Submit</button>
+
+            </form>
+        )   
+    } else {
+        return (
+            <div className="general-information">
+
+                <h1 className="section-title">General Information</h1>
+
+                <div className="section-data">
+
+                    <div className="data-item">
+                        <p className="prop-title">Name</p>
+                        <p>{generalInfo.name}</p>
+                    </div>
+
+                    <div className="data-item">
+                        <p className="prop-title">Email</p>
+                        <p>{generalInfo.email}</p>
+                    </div>
+
+                    <div className="data-item">
+                        <p className="prop-title">Phone number</p>
+                        <p>{generalInfo.phone}</p>
+                    </div>
+
+                </div>
+
+                <button onClick={handleEdit}>Edit</button>
+
+            </div>
+        )
+    }
+}
---
+++ src/components/PracticalExperience.jsx
@@ -0,0 +1,5 @@
+export function PracticalExperience() {
+    return (
+        <div>Practical Experience</div>
+    )
+}
